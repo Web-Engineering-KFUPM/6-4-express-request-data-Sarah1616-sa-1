0feec00764c443f755c57b3ecb8037200f4342d0
Sarah1616-sa <s202261060@kfupm.edu.sa>
2026-04-08 13:07:46 +0300

    Complete TODO 2: add /echo route

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -114,7 +114,31 @@ app.listen(3000, () => {
 });
 
 // create express app instance to create web server
+import express from "express";
 
+const app = express();
+
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
+
+  if (!name || !age) {
+    return res.status(400).json({
+      ok: false,
+      error: "name & age required",
+    });
+  }
+
+  return res.json({
+    ok: true,
+    name,
+    age,
+    msg: `Hello ${name}, you are ${age}`,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("API running at http://localhost:3000");
+});
 
 
 // Query params: /echo?name=Ali&age=22
